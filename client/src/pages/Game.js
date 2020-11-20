e04b578714b06baa59ff2ca1ee940cb55c7e5965
jsp <jonsp14@gmail.com>
2020-11-20 15:57:08 -0800

    add react-router

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Image from "../components/Image";
 import Text from "../components/Text";
 import story from "../story.json";
