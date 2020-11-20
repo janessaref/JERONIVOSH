e30f149e0b8d6891ce566ce8d617ee8879d69085
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-19 20:20:53 -0600

    made starter back end

---
+++ routes/api/story.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/storyControllers.js");
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const logger = require('morgan');
+// Requring all of our nessesary packages
+const express = require("express");
+const routes = require('./routes')
+// Setting up our port
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+// This allows us to recognize the incoming event's as json object. 
+app.use(express.json());
+app.use(logger('dev'));
+
+// Used to load the files that are in the directory public and recognizes them as static.
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+  // Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/JERONIVOSH");
+
+// requring our routes
+// app.use(routes);
+
+
+// listening for our ports
+app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+  
