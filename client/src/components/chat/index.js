66b8ad2ca43a09748d4e62a0b7e7ba2381751604
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-22 17:45:33 -0600

    dummy component

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,12 +1,41 @@
-import react, {userState, useEffect} from 'react'
-import Style from './style.css'
-import io from 'socket.io' 
-import TextField from '@material-ui/core/TextField' 
+import react, { userState, useEffect } from "react";
+import Style from "./style.css";
+import io from "socket.io";
+import TextField from "@material-ui/core/TextField";
 
-
-const socket = io.connect('http://localhost:3001')
+const socket = io.connect("http://localhost:3001");
 
 function Chat() {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = userState([]);
 
-
+  return (
+    <div>
+      <form onSubmit={onMessageSubmit}>
+        <h1>Messanger</h1>
+        <div className="name-field">
+          <TextField
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.name}
+            label="name"
+          />
+        </div>
+        <div>
+          <TextField
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.message}
+            id='outlined-multiline-static'
+            variant='outlined'
+            label="message"
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+     <div className='render-chat'></div>
+    </div>
+  );
 }
+
+export default Chat;
