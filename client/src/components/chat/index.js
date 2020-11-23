99fef465b268eab672b32dcd0a1329d943f46d0a
jsp <jonsp14@gmail.com>
2020-11-22 21:11:42 -0800

    fix chat

---
+++ client/src/components/chat/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import io from "socket.io-client";
+// import TextField from '@material-ui/core/TextField'
+// import TextField from "@material-ui/core/TextField";
+
+const socket = io.connect("http://localhost:3000");
+
+function Chat() {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState([]);
+
+
+const onTextChange = e => {
+    setState({...state, [e.target.name]: e.target.value})
+}
+
+useEffect(() => {
+    socket.on('message', ({name, message}) => {
+        setChat([...chat, {name, message}])
+    })
+})
+
+function onMessageSubmit(e) {
+    e.preventDefault();
+
+    const { name, message } = state
+    socket.emit('message', {name, message})
+    setState({message: '', name})
+}
+
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, i) => (
+      <div key={i}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="newForm">
+      <form>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <input
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.name}
+            label="name"
+          />
+        </div>
+        <div>
+          <textarea
+            name="message"
+            onChange={(e) => onTextChange(e)}
+            value={state.message}
+            label="message"
+          />
+        </div>
+        <button id="submitbutton" onClick={onMessageSubmit}>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>chat Log</h1>
+        {renderChat()}
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
