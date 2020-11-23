3510041a1488fbd40d12baeddb8efba5a212e8ee
jsp <jonsp14@gmail.com>
2020-11-22 21:10:02 -0800

    fix merge issue

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,73 +1,25 @@
 import React, { useState, useEffect } from "react";
-import "./style.css";
-import io from "socket.io-client";
-// import TextField from '@material-ui/core/TextField'
-// import TextField from "@material-ui/core/TextField";
-
-const socket = io.connect("http://localhost:3000");
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost/3001";
 
 function Chat() {
-  const [state, setState] = useState({ message: "", name: "" });
-  const [chat, setChat] = useState([]);
+    const [response, setResponse] = useState("");
 
+    useEffect(() => {
+        console.log(response);
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("FromAPI", data => {
+            setResponse(data);
+        });
+        return () => socket.disconnect();
+    }, []);
 
-const onTextChange = e => {
-    setState({...state, [e.target.name]: e.target.value})
-}
-
-useEffect(() => {
-    socket.on('message', ({name, message}) => {
-        setChat([...chat, {name, message}])
-    })
-})
-
-function onMessageSubmit(e) {
-    e.preventDefault();
-
-    const { name, message } = state
-    socket.emit('message', {name, message})
-    setState({message: '', name})
-}
-
-
-  const renderChat = () => {
-    return chat.map(({ name, message }, i) => (
-      <div key={i}>
-        <h3>
-          {name}: <span>{message}</span>
-        </h3>
-      </div>
-    ));
-  };
-
-  return (
-    <div className="newForm">
-      <form>
-        <h1>Messenger</h1>
-        <div className="name-field">
-          <input
-            name="name"
-            onChange={(e) => onTextChange(e)}
-            value={state.name}
-            label="name"
-          />
-        </div>
+    return (
         <div>
-          <textarea
-            name="message"
-            onChange={(e) => onTextChange(e)}
-            value={state.message}
-            label="message"
-          />
+            <input type="text" className="form-control" id="message" placeholder="Enter Message Here: " />
+            <p className="card bg-light">It's:{response}<time dateTime={response}>{response}</time></p>
         </div>
-        <button id="submitbutton" onClick={onMessageSubmit}>Send Message</button>
-      </form>
-      <div className="render-chat">
-        <h1>chat Log</h1>
-        {renderChat()}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Chat;
