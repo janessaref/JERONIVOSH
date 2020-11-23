28dd5bd54671ca48bef6626447f607678146ffde
jsp <jonsp14@gmail.com>
2020-11-22 21:20:37 -0800

    fix casing issues

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -7,7 +7,7 @@ import API from '../utils/api';
 import './style.css';
 import Signup from "../components/Signup";
 import Login from "../components/Login";
-import Chat from '../components/Chat/index.js'
+import Chat from '../components/Chat';
 import Credits from "../components/Credits"
 import Polls from "../components/Sidebar/polls"
 
