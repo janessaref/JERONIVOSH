a3e501f69e077b202659def8df7d918e90fd04bc
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-24 12:43:04 -0600

    changes to chat

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ". /style.css";
+// import ". /style.css";
 import io from "socket.io-client";
 // import TextField from '@material-ui/core/TextField'
 // import TextField from "@material-ui/core/TextField";
@@ -37,6 +37,7 @@ function onMessageSubmit(e) {
 //     socket.emit('message', {name, message})
 //     setState({message: '', name})
 // }
+
 
   const renderChat = () => {
     return chat.map(({ name, message }, i) => (
