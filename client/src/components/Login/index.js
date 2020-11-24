36cb1fa86d8bf87c26baa665e20b75e7bdbc55fb
Game Over <janessarefong@gmail.com>
2020-11-24 11:59:28 -0800

    edit styling for login page and add gif bg for homepage

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,27 +3,41 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 function login({ login }) {
-
-
     return (
-        <div className="container logIn">
+        <div className="container col-md-4 logIn">
             <div className="row">
-            <h1 className="float-left ml-3"> JERONIVOSH</h1>
+                <h1 className=" ml-3 px-auto brand mb-0">JERONIVOSH</h1>
             </div>
-            <div className="gameplay img-fluid">
-                <form id="signup" className="form-group signupForm" onSubmit={login}>
-                    <div className="form-group">
-                        <label>User Name: </label>
-                        <input type="text" className="form-control" id="userName" placeholder="Enter User Name: " />
+            <div className="gameplay container text-center">
+                <hr/>
+                <form id="loginForm" className="form-group mt-2" onSubmit={login}>
+                    <div className="form-group col-sm-10 mx-auto">
+                        <label>Username: </label>
+                        <input type="text" className="form-control" id="userName" placeholder="Enter your username" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-10 mx-auto">
                         <label>Password: </label>
-                        <input type="password" className="form-control" id="password" placeholder="Password" />
+                        <input type="password" className="form-control" id="password" placeholder="Enter your password" />
                     </div>
-                    <button id="submit" value="send" type="submit" className="btn">Submit</button>
-                    <Link className="button" to="/">Sign Up</Link>
-                    <Link className="button" to="/cooplogin">Multiplayer</Link>
-                    <Link className="button" to="/credits">Credits</Link>
+                    <button id="submit" value="send" type="submit" className="btn mt-2">Login
+                    </button>
+                    <div className ="row mx-auto text-center">
+                        <div className="col-md">
+                            <Link className="button" to="/">
+                                <button className="btn">Sign Up</button>
+                            </Link>
+                        </div>
+                       <div className="col-md">
+                            <Link className="button" to="/cooplogin">
+                                <button className="btn">Play Co-op</button>
+                            </Link>
+                       </div>
+                       
+                    </div>
+                    <div className ="row mx-auto text-center">
+                        
+                    {/* <Link className="button" to="/credits">Credits</Link> */}
+                    </div>
                 </form>
             </div>
         </div>
