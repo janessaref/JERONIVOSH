6e0e158778ddfd5715310bcb38bd57bf388da097
jsp <jonsp14@gmail.com>
2020-11-24 16:33:44 -0800

    fix chat path

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -7,7 +7,7 @@ import API from '../utils/api';
 import './style.css';
 import Signup from "../components/Signup";
 import Login from "../components/Login";
-import Chat from '../components/Chat';
+import Chat from '../components/Chat/index';
 import Credits from "../components/Credits";
 import Polls from "../components/Polls";
 import CoopLogin from "../components/CoopLogin";
