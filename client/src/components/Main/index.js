5719a7be6e7146f4874b93fa999cdc4911d8ae3e
Game Over <janessarefong@gmail.com>
2020-11-27 19:16:56 -0800

    fix styling and ids for buttons in main

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -10,46 +10,23 @@ function Main({start}) {
     <div className="container mainMenu">
         <div className="d-flex justify-content-center">
             <div class="card gameTypeContainer col-auto">
-            <div className="card-body">
-            <h1 className="px-auto mb-0 brand">
-                JERONIVOSH
-            </h1>
-            <hr/>
-            
-
-        
-            <div className="col-auto "></div>
-                {/* <Link to={"/game"}><div className="btn mr-1">SOLO</div></Link> */}
-                <button className="btn mr-1" onClick={start}>Single Player</button>
-                {/* <Link to={"/coopLogin"}> */}
-                        <div className="btn ml-1">CO-OP (AVAILABLE SOON)</div>
-                 {/* </Link> */}
-                
+                <div className="card-body">
+                    <h1 className="px-auto mb-0 brand">
+                    JERONIVOSH
+                    </h1>
+                    <hr/>
+                        {/* <Link to={"/game"}><div className="btn mr-1">SOLO</div></Link> */}
+                        <button className="btn mr-1" id="soloBtn" onClick={start}>Single Player</button>
+                        {/* <Link to={"/coopLogin"}> */}
+                        <div className="btn ml-1" id ="coopBtn" >CO-OP (AVAILABLE SOON)</div>
+                        {/* </Link> */}
+                </div> 
             </div>
         </div>
-
     </div>
    ) 
 }
 
 export default withRouter(Main);
 
-<div className="container mainMenu">
-<div className="d-flex justify-content-center">
-    <div class="card gameTypeContainer col-auto">
-        <div className="card-body">
-            <h1 className="px-auto mb-0 brand">
-                JERONIVOSH
-            </h1>
-            <hr/>
-            <Link to={"/game"}>
-                <div className="btn mr-1" id="soloBtn">SOLO</div>
-            </Link>
-            {/* <Link to={"/coopLogin"}> */}
-                <div className="btn ml-1" id="coopBtn">CO-OP (AVAILABLE SOON)</div>
-            {/* </Link> */}
-        </div>
-    </div>
-</div>
-</div>
 
