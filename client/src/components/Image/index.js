feca5c9111d2b6a5b066bbdc3374ce4250a81b1f
jsp <jonsp14@gmail.com>
2020-11-29 15:52:14 -0800

    add loading icon to image

--- client/src/components/Image/index.js
+++ client/src/components/Image/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./style.css";
 
-function Image({ user, story }) {
+function Image({ user, story, spinner }) {
   const [levels, setLevels] = useState({ status: "unmuted" });
   const [visibleX, setVisibleX] = useState(false);
+  // loading icon
+  const [loading, setLoading] = useState(true);
 
-  let style={display: "block"};
-  if(!visibleX) style.display = "none";
+  let style = { display: "block" };
+  if (!visibleX) style.display = "none";
+
+  function spinner() {
+    setLoading(false);
+  }
 
   function showXImage() {
     setVisibleX(true);
@@ -45,8 +51,8 @@ function Image({ user, story }) {
   return (
     <div>
       <div className="mute fixed">
-        <img src="./assets/redx.png" className="redX" style={style} onClick={handleMute}/>
-        <img src="./assets/audiosmall.png" onClick={handleMute} id="volIcon"/>
+        <img src="./assets/redx.png" className="redX" style={style} onClick={handleMute} />
+        <img src="./assets/audiosmall.png" onClick={handleMute} id="volIcon" />
       </div>
       <div className="lives">
         <p>Lives: {user.lives}</p>
@@ -54,7 +60,12 @@ function Image({ user, story }) {
       <audio className="float-right" autoPlay loop id="gameAudio" ref={volume}>
         <source src="./assets/4_29.mp3" type="audio/mpeg" />
       </audio>
-      <img className="gif img-fluid" src={story[user.level].image}></img>
+      <div className="gif img-fluid" style={{ display: loading ? "block" : "none" }}>
+        Loading Images
+    </div>
+      <div style={{ display: loading ? "none" : "block" }}>
+        <img className="gif img-fluid" src={story[user.level].image} onLoad={spinner}></img>
+      </div>
     </div>
   );
 }
