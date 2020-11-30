cb5db10fcfcac0f72e6c40b230eed9a09e65fab9
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-29 17:00:58 -0800

    fixing text

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -28,10 +28,10 @@ function signup({ signup, authorized, userMessage, passMessage }) {
                             <input type="password" className="form-control" id="password" placeholder="Create password" />
                         </div>
                         <div className={userMessage}>
-                           <p>that user Name is Taken</p>
+                           <p>User Name is Taken</p>
                         </div>
                         <div className={passMessage}>
-                            <p>Sorry password should be <br /> atleast 8 characters long</p>
+                            <p>Password must be <br /> atleast 8 characters</p>
                         </div>
                         <div className ="row mx-auto text-center">
                             <div className="col-md">
