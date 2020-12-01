1c70d6242a150319d5133c749a8cd20348a22f4b
Game Over <janessarefong@gmail.com>
2020-12-01 00:03:20 -0800

    fix styling and change to invalid

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -28,7 +28,7 @@ function Login({ login, message, input, erase }) {
                                 <input type="password" className={input} id="password" placeholder="Enter your password" />
                             </div>
                             <div className={message}>
-                                    <p>Wrong username or password.</p>
+                                    <p>Invalid username or password.</p>
                                 </div>
                             <div className ="row mx-auto text-center">
                                 <div className="col-md">
